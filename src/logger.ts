c13383812e92141d77c14e96d25fd65b187f4210
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-04-22 17:11:23 -0400

    Updated logger and logs path

--- src/logger.ts
+++ src/logger.ts
@@ -3,7 +3,7 @@ import colors from 'colors';
 
 colors.enable();
 
-const root = __dirname;
+const root: string = process.env.PN_MIIVERSE_API_LOGGER_PATH ? process.env.PN_MIIVERSE_API_LOGGER_PATH : `${__dirname}/..`;
 fs.ensureDirSync(`${root}/logs`);
 
 const streams = {
@@ -12,34 +12,34 @@ const streams = {
 	error: fs.createWriteStream(`${root}/logs/error.log`),
 	warn: fs.createWriteStream(`${root}/logs/warn.log`),
 	info: fs.createWriteStream(`${root}/logs/info.log`)
-};
+} as const;
 
-export function LOG_SUCCESS(input) {
-	const time = new Date();
+export function LOG_SUCCESS(input: string): void {
+	const time: Date = new Date();
 	input = `[${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}] [SUCCESS]: ${input}`;
 	streams.success.write(`${input}\n`);
 
 	console.log(`${input}`.green.bold);
 }
 
-export function LOG_ERROR(input) {
-	const time = new Date();
+export function LOG_ERROR(input: string): void {
+	const time: Date = new Date();
 	input = `[${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}] [ERROR]: ${input}`;
 	streams.error.write(`${input}\n`);
 
 	console.log(`${input}`.red.bold);
 }
 
-export function LOG_WARN(input) {
-	const time = new Date();
+export function LOG_WARN(input: string): void {
+	const time: Date = new Date();
 	input = `[${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}] [WARN]: ${input}`;
 	streams.warn.write(`${input}\n`);
 
 	console.log(`${input}`.yellow.bold);
 }
 
-export function LOG_INFO(input) {
-	const time = new Date();
+export function LOG_INFO(input: string): void {
+	const time: Date = new Date();
 	input = `[${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}] [INFO]: ${input}`;
 	streams.info.write(`${input}\n`);
 
