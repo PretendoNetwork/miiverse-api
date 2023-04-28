9a25a7a9a22a266aa4df1723ab919f564889dd05
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-04-28 16:36:20 -0400

    Added test and dev subdomains to discovery

--- src/services/discovery/index.ts
+++ src/services/discovery/index.ts
@@ -13,6 +13,8 @@ const discovery = express.Router();
 // Create subdomains
 LOG_INFO('[MIIVERSE] Creating \'discovery\' subdomain');
 router.use(subdomain('discovery.olv', discovery));
+router.use(subdomain('discovery-test.olv', discovery));
+router.use(subdomain('discovery-dev.olv', discovery));
 
 // Setup routes
 discovery.use('/v1/endpoint', discoveryHandlers);
