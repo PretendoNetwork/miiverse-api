ba066f1c1812d411e81db2ed51f6c7a6d63a95e6
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-06-13 17:07:17 -0400

    calculateMostPopularCommunities is now more clear and faster

--- src/services/api/routes/topics.ts
+++ src/services/api/routes/topics.ts
@@ -114,46 +114,46 @@ async function generateTopicsXML(communities: HydratedCommunityDocument[]): Prom
 async function calculateMostPopularCommunities(hours: number, limit: number): Promise<HydratedCommunityDocument[]> {
 	const now: Date = new Date();
 	const last24Hours: Date = new Date(now.getTime() - hours * 60 * 60 * 1000);
-	const posts: HydratedPostDocument[] = await Post.find({ created_at: { $gte: last24Hours },  message_to_pid: null });
 
-	if (!posts.length) {
-		return [];
-	}
-
-	const communityIDCounts: {
-		[key: string]: number
-	} = {};
-
-	for (const post of posts) {
-		const communityID: string = post.community_id;
-		communityIDCounts[communityID] = (communityIDCounts[communityID] || 0) + 1;
-	}
-
-	const popularCommunitiesSorted: string[] = Object.entries(communityIDCounts)
-		.sort((a, b) => (b[1] as number) - (a[1] as number))
-		.map((entry) => entry[0]);
-
-	if (popularCommunitiesSorted.length < limit) {
-		return Community.find().limit(limit).sort({
-			followers: -1
-		});
-	}
-
-	const response: HydratedCommunityDocument[] = await Community.aggregate([
-		{ $match: { olive_community_id: { $in: popularCommunitiesSorted }, parent: null } },
-		{$addFields: {
-			index: { $indexOfArray: [ popularCommunitiesSorted, '$olive_community_id' ] }
-		}},
-		{ $sort: { index: 1 } },
-		{ $limit : limit },
-		{ $project: { index: 0, _id: 0 } }
+	const popularCommunities: {
+		_id: string;
+		count: number;
+	}[] = await Post.aggregate([
+		{
+			$match: {
+				created_at: {
+					$gte: last24Hours
+				},
+				message_to_pid: null
+			}
+		},
+		{
+			$group: {
+				_id: '$community_id',
+				count: {
+					$sum: 1
+				}
+			}
+		},
+		{
+			$limit: limit
+		},
+		{
+			$sort: {
+				count: -1
+			}
+		}
 	]);
 
-	if (response.length < limit) {
+	if (popularCommunities.length < limit) {
 		return calculateMostPopularCommunities(hours + hours, limit);
-	} else {
-		return response;
 	}
+
+	return Community.find({
+		olive_community_id: {
+			$in: popularCommunities.map(({ _id }) => _id)
+		}
+	});
 }
 
 export default router;
