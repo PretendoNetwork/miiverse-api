60765369962b8f845468b710c17ddf31ec13ae51
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-10-02 13:00:23 -0400

    fixed spacing issue in Post type def

--- src/types/mongoose/post.ts
+++ src/types/mongoose/post.ts
@@ -5,38 +5,38 @@ import { PostPainting, PostScreenshot } from '@/types/common/post';
 
 export interface IPost {
 	id: string;
-    title_id: string;
-    screen_name: string;
-    body: string;
-    app_data: string;
-    painting: string;
-    screenshot: string;
-    screenshot_length: number;
-    search_key: string[];
-    topic_tag: string;
-    community_id: string;
-    created_at: Date;
-    feeling_id: number;
-    is_autopost: number;
-    is_community_private_autopost?: number;
-    is_spoiler: number;
-    is_app_jumpable: number;
-    empathy_count?: number;
-    country_id: number;
-    language_id: number;
-    mii: string;
-    mii_face_url: string;
-    pid: number;
-    platform_id: number;
-    region_id: number;
-    parent: string;
-    reply_count?: number;
-    verified: boolean;
-    message_to_pid?: string;
-    removed: boolean;
-    removed_reason?: string;
-    yeahs?: Types.Array<number>;
-    number?: number;
+	title_id: string;
+	screen_name: string;
+	body: string;
+	app_data: string;
+	painting: string;
+	screenshot: string;
+	screenshot_length: number;
+	search_key: string[];
+	topic_tag: string;
+	community_id: string;
+	created_at: Date;
+	feeling_id: number;
+	is_autopost: number;
+	is_community_private_autopost?: number;
+	is_spoiler: number;
+	is_app_jumpable: number;
+	empathy_count?: number;
+	country_id: number;
+	language_id: number;
+	mii: string;
+	mii_face_url: string;
+	pid: number;
+	platform_id: number;
+	region_id: number;
+	parent: string;
+	reply_count?: number;
+	verified: boolean;
+	message_to_pid?: string;
+	removed: boolean;
+	removed_reason?: string;
+	yeahs?: Types.Array<number>;
+	number?: number;
 }
 
 export interface IPostMethods {
@@ -44,14 +44,14 @@ export interface IPostMethods {
 	downReply(): Promise<void>;
 	remove(reason: string): Promise<void>;
 	unRemove(reason: string): Promise<void>;
-    generatePostUID(length: number): Promise<void>;
+	generatePostUID(length: number): Promise<void>;
 	cleanedBody(): string;
 	cleanedMiiData(): string;
 	cleanedPainting(): string;
 	cleanedAppData(): string;
 	formatPainting(): PostPainting | undefined;
 	formatScreenshot(): PostScreenshot | undefined;
-    json(options: PostToJSONOptions, community?: HydratedCommunityDocument): Record<string, any>;
+	json(options: PostToJSONOptions, community?: HydratedCommunityDocument): Record<string, any>;
 }
 
 interface IPostQueryHelpers {}
